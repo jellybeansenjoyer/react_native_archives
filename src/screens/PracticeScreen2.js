aba50d3f2aed61a5df6f4a36677fd967b446afe8
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-10-20 00:25:20 +0530

    check

--- src/screens/PracticeScreen2.js
+++ src/screens/PracticeScreen2.js
@@ -3,24 +3,43 @@ import {View,StyleSheet,Button,Text} from 'react-native'
 import ReusableComp from './ReusableComp'
 
 const screen = ()=>{
+    const change = (value,factor)=>{
+        switch (value) {
+            case 'red': if(red + factor > 255 || red + factor < 0)
+                        return
+                        else 
+                        setRed(red+factor)
+                        return
+            case 'blue': if (blue + factor > 255 || blue + factor < 0)
+                return
+            else
+                setBlue(blue + factor)
+                return
+            case 'green': if (green + factor > 255 || green + factor < 0)
+                return
+            else
+                setGreen(green + factor)
+                return
+        }
+    }
     const FACTOR = 20
     const [red,setRed] = useState(0);
     const [blue,setBlue] = useState(0);
     const [green,setGreen] = useState(0);
 return (<View>
         <ReusableComp name="Red" increase= {()=>{
-            setRed(red+FACTOR)
+            change('red',FACTOR)
         }} decrease={()=>{
-            setRed(red-FACTOR)
+            change('red',FACTOR*-1)
         }} />
     <ReusableComp name="Green" increase={() => {
-        setGreen(green + FACTOR)
+        change("green",FACTOR)
     }} decrease={() => {
-        setGreen(green - FACTOR) }} />
+        change("green", FACTOR*-1) }}/>
     <ReusableComp name="Blue" increase={() => {
-        setBlue(blue + FACTOR)
+        change("blue",FACTOR)
     }} decrease={() => {
-        setBlue(blue - FACTOR)
+        change("blue", FACTOR*-1)
     }} />
     <View style={{height:100 , width:100, backgroundColor:`rgb(${red},${green},${blue})`}} />
     </View>)
