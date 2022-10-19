bd31e656e9b2de5dd3b7046a1c5d0bc5a604fb26
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-10-20 00:15:30 +0530

    factor variable

--- src/screens/PracticeScreen2.js
+++ src/screens/PracticeScreen2.js
@@ -3,23 +3,24 @@ import {View,StyleSheet,Button,Text} from 'react-native'
 import ReusableComp from './ReusableComp'
 
 const screen = ()=>{
+    const FACTOR = 20
     const [red,setRed] = useState(0);
     const [blue,setBlue] = useState(0);
     const [green,setGreen] = useState(0);
 return (<View>
         <ReusableComp name="Red" increase= {()=>{
-            setRed(red+20)
+            setRed(red+FACTOR)
         }} decrease={()=>{
-            setRed(red-20)
+            setRed(red-FACTOR)
         }} />
     <ReusableComp name="Green" increase={() => {
-        setGreen(green + 20)
+        setGreen(green + FACTOR)
     }} decrease={() => {
-        setGreen(green - 20) }} />
+        setGreen(green - FACTOR) }} />
     <ReusableComp name="Blue" increase={() => {
-        setBlue(blue + 20)
+        setBlue(blue + FACTOR)
     }} decrease={() => {
-        setBlue(blue - 20)
+        setBlue(blue - FACTOR)
     }} />
     <View style={{height:100 , width:100, backgroundColor:`rgb(${red},${green},${blue})`}} />
     </View>)
--- src/screens/ReusableComp.js
+++ src/screens/ReusableComp.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {View,StyleSheet,Button,Text} from 'react-native'
 
-const ReusableComp = ({ name , increase ,decrease})=>{
+const ReusableComp = (props)=>{
+    
     return (
         <View style={{marginTop:30 , marginBottom:30}}>
             <Text>{name}</Text>
-            <Button title= {`Increase ${name}`} onPress={()=>{
-                   increase()
+            <Button title= {`Increase ${props.name}`} onPress={()=>{
+                   props.increase()
             }}  />
             <Button title= {`Decraese ${name}`} onPress={()=>{
-                    decrease()
+                    props.decrease()
             }}  />
         </View>
     )
