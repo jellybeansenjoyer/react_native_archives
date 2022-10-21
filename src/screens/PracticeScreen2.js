e5392ba8fa60a7dd1540dd0a6adeaecec34e79b3
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-10-22 00:13:15 +0530

    using useReducer to make changes to the screen

--- src/screens/PracticeScreen2.js
+++ src/screens/PracticeScreen2.js
@@ -1,48 +1,30 @@
-import React,{useState} from 'react'
+import React,{useReducer, useState} from 'react'
 import {View,StyleSheet,Button,Text} from 'react-native'
 import ReusableComp from './ReusableComp'
-
-const screen = ()=>{
-    const change = (value,factor)=>{
-        switch (value) {
-            case 'red': if(red + factor > 255 || red + factor < 0)
-                        return
-                        else 
-                        setRed(red+factor)
-                        return
-            case 'blue': if (blue + factor > 255 || blue + factor < 0)
-                return
-            else
-                setBlue(blue + factor)
-                return
-            case 'green': if (green + factor > 255 || green + factor < 0)
-                return
-            else
-                setGreen(green + factor)
-                return
-        }
+const reducer = (state, action) => {
+    switch (action.colorToChange) {
+        case 'Red':  if(state.red+action.amount<0||state.red+action.amount>255)
+                     return state
+                     return { ...state, red: state.red + action.amount };
+        case 'Green': if (state.red + action.amount < 0 || state.red + action.amount > 255)
+                    return state 
+                    return { ...state, green: state.green + action.amount };
+        case 'Blue': if (state.red + action.amount < 0 || state.red + action.amount > 255)
+                     return state
+                    return { ...state, blue: state.blue + action.amount };
+        default: return state;
     }
+}
+const screen = ()=>{
     const FACTOR = 20
-    const [red,setRed] = useState(0);
-    const [blue,setBlue] = useState(0);
-    const [green,setGreen] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+   
+    
 return (<View>
-        <ReusableComp name="Red" increase= {()=>{
-            change('red',FACTOR)
-        }} decrease={()=>{
-            change('red',FACTOR*-1)
-        }} />
-    <ReusableComp name="Green" increase={() => {
-        change("green",FACTOR)
-    }} decrease={() => {
-        change("green", FACTOR*-1) }}/>
-
-    <ReusableComp name="Blue" increase={() => {
-        change("blue",FACTOR)
-    }} decrease={() => {
-        change("blue", FACTOR*-1)
-    }} />
-    <View style={{height:100 , width:100, backgroundColor:`rgb(${red},${green},${blue})`}} />
+    <ReusableComp name="Red" increase={() => { dispatch({ colorToChange: 'Red', amount: FACTOR }) }} decrease={() => { dispatch({ colorToChange: 'Red', amount: FACTOR*-1 }) }} />
+    <ReusableComp name="Green" increase={() => { dispatch({ colorToChange: 'Green', amount: FACTOR }) }} decrease={() => { dispatch({ colorToChange: 'Green', amount: FACTOR*-1 }) }}/>
+    <ReusableComp name="Blue" increase={() => { dispatch({ colorToChange: 'Blue', amount: FACTOR })  }} decrease={() => { dispatch({ colorToChange: 'Blue', amount: FACTOR*-1 }) }} />
+    <View style={{height:100 , width:100, backgroundColor:`rgb(${state.red},${state.green},${state.blue})`}} />
     </View>)
 }
 
